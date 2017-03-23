3f816bc8297c7ccd0af89cd6b001d5bf90ff2f8c
René Pavlík <rene.pavlik@braiins.cz>
2017-03-23 13:13:37 +0100

    Version 3.2 repo has different gpg signature

--- manifests/init.pp
+++ manifests/init.pp
@@ -41,7 +41,10 @@ class zabbix($version = $zabbix::params::version,
       location   => "http://repo.zabbix.com/zabbix/${version}/debian",
       repos      => 'main',
       release    => $release,
-      key        => 'FBABD5FB20255ECAB22EE194D13D58E479EA5ED4',
+      key        => $version ? {
+        default => 'FBABD5FB20255ECAB22EE194D13D58E479EA5ED4',
+        '3.2'   => 'A1848F5352D022B9471D83D0082AB56BA14FE591',
+      },
       key_server => 'pgp.mit.edu',
     }
   }
